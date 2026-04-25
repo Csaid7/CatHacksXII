8ed8fb2d8b70f4290494669d7dbf9582122843f7
KiaraJohnson2004 <princesskiarajohnson04@gmail.com>
2026-04-25 15:26:38 -0400

    Added hit animations

--- Player.gd
+++ Player.gd
@@ -46,7 +46,6 @@ func _physics_process(delta):
 		state = STATES.RUN
 		animation.play("run")
 	else:
-		#if state != STATES.HIT:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if state == STATES.RUN:
 			state = STATES.IDLE
@@ -56,21 +55,29 @@ func _physics_process(delta):
 
 
 func _on_animated_sprite_2d_animation_finished():
-	if state == STATES.JUMP or state == STATES.PUNCH:
+	if (state == STATES.JUMP or state == STATES.PUNCH) or state == STATES.HIT:
+		print("animation finished")
 		state = STATES.IDLE
 
 
 func _on_punch_hitbox_body_entered(body):
 	if body != self and body.has_method("apply_knockback"):
 		var attack_dir = Vector2(facing, 0)
-		body.apply_knockback(attack_dir)
+		var attacker_pos = global_position
+		body.apply_knockback(attack_dir,attacker_pos)
 
 
 func _on_punch_timer_timeout():
 	punchHitBox.monitoring = false
 
-func apply_knockback(attack_dir):
+func apply_knockback(attack_dir,attacker_pos):
+	state = STATES.HIT
+	var to_attacker = (attacker_pos - global_position).normalized()
+	var dot = to_attacker.x * facing
+	if dot < 0:
+		animation.play("suckerPunched")
+	else:
+		animation.play("hit")
 	velocity.x += attack_dir.x * 400
 	velocity.y = -200
 	move_and_slide()
-	state = STATES.IDLE
